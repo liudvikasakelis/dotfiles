fef8b45a5e6de93b6bcad5c4f611cf750c519a03
Liudvikas Akelis <liudvikas.akelis@nasdaq.com>
2024-05-19 21:16:42 +0300

    powershell is now part of the family

--- .emacs.d/lisp/local-powershell.el
+++ .emacs.d/lisp/local-powershell.el
@@ -1,3 +1,7 @@
 (provide 'local-powershell)
+
+(use-package powershell
+  :ensure t)
+
 (add-to-list 'auto-mode-alist '("\\.ps1\\'" . powershell-mode))
 (add-hook 'powershell-mode-hook (lambda () (setq show-trailing-whitespace t)))
--- .emacs.d/lisp/local-sql.el
+++ .emacs.d/lisp/local-sql.el
@@ -1,5 +1,10 @@
 (provide 'local-sql)
+
+(use-package sqlup-mode
+  :ensure t)
+
 (setq sqlind-basic-offset 4)
 (add-hook 'sql-mode-hook 'sqlind-minor-mode)
 (add-hook 'sql-mode-hook 'sqlup-mode)
 (setq sql-ms-program "sqlcmd")
+
