94bb89887b2792f466c5b692068db21de24e2bb0
Liudvikas Akelis <liudvikas.akelis@nasdaq.com>
2024-05-21 12:34:21 +0300

    local python

--- .emacs.d/lisp/local-python.el
+++ .emacs.d/lisp/local-python.el
@@ -1,5 +1,10 @@
 (provide 'local-python)
 
+(use-package elpy
+  :ensure t)
+(use-package lsp
+  :ensure t)
+
 (elpy-enable)
 
 (require 'lsp-mode)
@@ -7,7 +12,7 @@
 (add-hook 'python-mode-hook 'pyvenv-mode)
 (add-hook 'python-mode-hook 'elpy-mode)
 
-(setq python-shell-interpreter "ipython3"
+(setq python-shell-interpreter "python3.exe"
       python-shell-interpreter-args
       "-i --simple-prompt --InteractiveShell.display_page=True"
       lsp-pylsp-plugins-flake8-enabled nil
